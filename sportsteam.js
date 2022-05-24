e55963327b17c1f2874fca2da734a24aa6070739
crystgandhi <105329072+crystgandhi@users.noreply.github.com>
2022-05-24 08:01:02 +0530

    Create sportsteam.js

---
+++ sportsteam.js
@@ -0,0 +1,49 @@
+import json
+
+cricket='''
+{
+
+ "twenty20CricketTeams"; [
+
+	{
+		"Team": "Chennai Super Kings",
+		"Captain": "Mahendra Singh Dhoni",
+		"Coach": "Stephen Fleming",
+		"Owner": "India Cements",
+		"City": "Chennai",
+		"official website": "chennaisuperkings.com",
+		"championship": "2"
+	},
+
+	{
+		"Team": "Mumbai Indians",
+		"Captain": "Rohit Sharma",
+		"Coach": "Mahela Jayawardene",
+		"Owner": "Reliance Industries",
+		"City": "Mumbai",
+		"official website": "mumbaiindians.com",
+		"championship": 2
+	},
+	
+	{
+		"Team": "Rajasthan royals",
+		"Captain": "Sanju Samson",
+		"Coach": "Kumar Sangakkara",
+		"Owner": "Manoj Badale",
+		"City": "Jaipur",
+		"official website": "rajasthanroyals.com",
+		"championship": 0
+	}.
+
+{
+	"Team": "Kolkata Knight Riders",
+	"Captain": "Shreyas Iyer",
+	"Coach": "Brendon McCullum",
+	"Owner": "Knight Riders Group",
+	"City": "Kolkata",
+	"official website": "kkr.in",
+	"championship": 0
+	}
+]
+}
+'''
